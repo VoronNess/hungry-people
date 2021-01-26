36593a6f5ae5277b44b92e713c8ad1e18b27761a
VoronNess <64578357+VoronNess@users.noreply.github.com>
2021-01-26 20:06:14 +0300

    валидация форм, немного анимации

---
+++ исходники/js/scripts.js
@@ -0,0 +1,108 @@
+//Open and close mobile menu
+
+const menuOpenBtn = document.querySelector(".header__nav-toggle");
+const menu = document.querySelector(".header__nav");
+const menuContentOpened = document.querySelector(".header__nav--opened");
+const menuContentClosed = document.querySelector(".header__nav--closed");
+let menuShow = true;
+
+
+menuOpenBtn.addEventListener('click', () => {
+  if (menuShow) {
+    menu.classList.remove("header__nav--closed");
+    menu.classList.add("header__nav--opened");
+    menuShow = false;
+
+  } else {
+    menu.classList.remove("header__nav--opened");
+    menu.classList.add("header__nav--closed");
+    menuShow = true;
+  }
+})
+
+//Specials-slider
+const dots = document.querySelectorAll(".dot");
+const slidesList = document.querySelector('.specials__list');
+const liveSlides = slidesList.children;
+const classCurrentDot = 'dot__current';
+const classCurrentSpecial = 'slide__current';
+
+const showCurrentItem = (i, collection, classChange) => {
+  for (const item of collection) {
+    item.classList.remove(classChange);
+  }
+  collection[i].classList.add(classChange);
+};
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    let currentSlide = indexDot;
+    showCurrentItem(currentSlide, liveSlides, classCurrentSpecial);
+    showCurrentItem(currentSlide, dots, classCurrentDot);
+  });
+});
+
+//Menu-table-slider
+const menuCategories = document.querySelectorAll(".menu__nav");
+const menuSlidesList = document.querySelector('.menu__slide-list');
+const menuliveSlides = menuSlidesList.children;
+const classCurrentMenu = 'menu__current';
+const classCurrentCategory = 'menu__nav-current';
+
+menuCategories.forEach((item, indexCategory) => {
+  item.addEventListener('click', () => {
+    let menuCurrentSlide = indexCategory;
+    showCurrentItem(menuCurrentSlide, menuliveSlides, classCurrentMenu);
+    showCurrentItem(menuCurrentSlide, menuCategories, classCurrentCategory);
+  });
+});
+
+//Validation and Notice for Forms
+
+const bookingForm = document.querySelector(".booking__form");
+const telInputBooking = document.querySelector('.booking__form--tel');
+
+const contactsForm = document.querySelector(".contacts__form");
+const telInputContacts = document.querySelector(".contacts__form--tel");
+const rightTelSymbols = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{6,10}$/;
+
+const successNotice = document.querySelector(".modal__booking-success");
+const closeNoticeBtn = successNotice.querySelector(".modal__btn-close");
+
+closeNoticeBtn.addEventListener("click", ()=>{
+  successNotice.classList.remove("modal-show");
+})
+
+bookingForm.addEventListener("submit",(evt) => {
+  evt.preventDefault();
+  successNotice.classList.add("modal-show");
+  bookingForm.reset();
+});
+
+contactsForm.addEventListener("submit",(evt) => {
+  evt.preventDefault();
+  successNotice.classList.add("modal-show");
+  contactsForm.reset();
+});
+
+telInputBooking.addEventListener("input",() => {
+  if (!rightTelSymbols.test(telInputBooking.value)) {
+    telInputBooking.setCustomValidity("введите действующий телефон в формате +774377634574, 84848434343 или 454545");
+    telInputBooking.reportValidity();
+
+  } else {
+
+    telInputBooking.setCustomValidity(``);
+}
+});
+
+telInputContacts.addEventListener("input",() => {
+  if (!rightTelSymbols.test(telInputContacts.value)) {
+    telInputContacts.setCustomValidity("введите действующий телефон в формате +774377634574, 84848434343 или 454545");
+    telInputContacts.reportValidity();
+
+  } else {
+
+    telInputContacts.setCustomValidity(``);
+}
+});
